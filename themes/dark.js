855eae7912c9d75b81a7c8f5ec9ac282230dc0b6
Diego Figueroa <diego@dfigueroa.me>
2020-12-13 21:58:44 -0400

    refactor(themes): favor js themes over css

---
+++ themes/dark.js
@@ -0,0 +1,12 @@
+export default {
+  background: {
+    main: '#1a1a1a',
+    light: '#333333',
+    retry: '#1a1a1a',
+    error: 'rgba(198, 38, 46, 0.3)',
+  },
+  text: '#fafafa',
+  highlight: '#a56de2',
+  correct: '#68b723',
+  wrong: '#c6262e'
+}
---
+++ themes/light.js
@@ -0,0 +1,12 @@
+export default {
+  background: {
+    main: '#fafafa',
+    light: '#eaeaea',
+    retry: '#d4d4d4',
+    error: 'rgba(198, 38, 46, 0.3)',
+  },
+  text: '#1a1a1a',
+  highlight: '#a56de2',
+  correct: '#68b723',
+  wrong: '#c6262e'
+}

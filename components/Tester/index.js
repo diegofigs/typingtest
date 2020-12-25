58aabba814f4e8602c5abae63adfcebb0383ec5e
Diego Figueroa <diego@dfigueroa.me>
2020-12-25 05:44:26 -0400

    :sparkles: (components/Tester) use time interval to build up events log

--- components/Tester/index.js
+++ components/Tester/index.js
@@ -1,9 +1,10 @@
 import { Fragment, useCallback, useEffect, useMemo, useState } from 'react';
 import classNames from 'classnames';
 import randomWords from 'random-words';
-import { addSeconds, isBefore, isWithinInterval } from 'date-fns';
 
 import styles from './Tester.module.css';
+import { useInterval } from '@hooks';
+import { calculateWPM, calculateNetWPM, calculateAccuracy, calculateCorrectKeys } from '@core/equations';
 
 const options = [10, 25, 50, 100];
 
@@ -18,46 +19,38 @@ export default function Tester({ theme }) {
     return words.join(' ');
   }, [words]);
 
-  const [events, setEvents] = useState([]);
   const [input, setInput] = useState('');
-  const onKeyDown = useCallback((event) => {
-    const { key, timeStamp } = event;
-    setEvents(events => [...events, { key, timeStamp }]);
-  }, [input, text]);
-
   const [startTime, setStartTime] = useState();
   useEffect(() => {
     if (input.length === 1 && !startTime) setStartTime(Date.now());
   }, [input, startTime]);
-
-  const [wpm, setWPM] = useState();
+  
+  const [, setWPM] = useState();
   const [netWPM, setNetWPM] = useState();
-  useEffect(() => {
-    if (startTime) {
-      const finishTime = (Date.now() - startTime) / 60000; // convert to minutes: 1s / 1000ms * 1m / 60s
-      const grossWords = input.length / 5;
-      setWPM(Math.floor(grossWords / finishTime));
-
-      const uncorrectedErrors = input.length - computeCorrectKeys(input, text);
-      setNetWPM(Math.floor((grossWords - uncorrectedErrors) / finishTime));
-    }
-  }, [input, text, startTime]);
-
   const [accuracy, setAccuracy] = useState();
   useEffect(() => {
     if (text.length !== 0 && text.length === input.length) {
-      const correctKeys = computeCorrectKeys(input, text)
-      setAccuracy(((correctKeys / text.length) * 100).toFixed(2));
+      const finishTime = Date.now();
+      setWPM(calculateWPM(input, startTime, finishTime));
+      setNetWPM(calculateNetWPM(input, text, startTime, finishTime));
+      setAccuracy((calculateAccuracy(input, text)).toFixed(2));
     }
-  }, [input, text]);
+  }, [input, text, startTime]);
+
+  const [events, setEvents] = useState([]);
+  useInterval(() => {
+    const t = Date.now();
+    const raw = calculateWPM(input, startTime, t);
+    const wpm = calculateNetWPM(input, text, startTime, t);
+    const errors = input.length - calculateCorrectKeys(input, text);
+    const stats = { t, wpm, errors, raw };
+    setEvents(events => [...events, stats]);
+  }, startTime && !accuracy ? 1000 : null);
   useEffect(() => {
     if (accuracy) {
       const finishTime = Date.now();
-      for (let interval = startTime, second = 1; isBefore(interval, finishTime); interval = addSeconds(interval, 1), second++) {
-        const eventsInSlot = events.filter(({ timeStamp }) => isWithinInterval(timeStamp, { start: interval, end: addSeconds(interval, 1) }));
-      }
     }
-  }, [accuracy, events, startTime]);
+  }, [accuracy, events]);
 
   const reset = useCallback((amount) => {
     const generatedWords = generate(amount);
@@ -77,7 +70,7 @@ export default function Tester({ theme }) {
           {index !== options.length - 1 ? ' / ' : ''}
         </Fragment>;
       })}</div>
-      <span className="stats">WPM: {netWPM ? netWPM : 'XX'} ACC: {accuracy ? `${accuracy}%` : 'XX'}</span>
+      <span className="stats">WPM: {netWPM ? netWPM : 'XX'} ACC: {accuracy ? `${accuracy} %` : 'XX'}</span>
     </div>
     <div className={classNames(styles.area, 'area')}>
       <div className={styles.display}>
@@ -98,7 +91,7 @@ export default function Tester({ theme }) {
         })}
       </div>
       <div className={styles.bar}>
-        <input disabled={input.length === text.length} className={styles.input} value={input} onChange={e => setInput(e.target.value)} onKeyDown={onKeyDown} type="text" spellCheck="false" autoComplete="off" autoCorrect="off" autoCapitalize="off" tabIndex="0" />
+        <input disabled={input.length === text.length} className={styles.input} value={input} onChange={e => setInput(e.target.value)} type="text" spellCheck="false" autoComplete="off" autoCorrect="off" autoCapitalize="off" tabIndex="0" />
         <button className={classNames(styles.retry, 'retry')} onClick={() => reset(wordAmount)}>Retry</button>
       </div>
     </div>
@@ -143,10 +136,6 @@ export default function Tester({ theme }) {
       }
     `}</style>
   </div>
-}
-
-function computeCorrectKeys(input, text) {
-  return [...input].reduce((total, char, index) => char === text[index] ? total + 1 : total, 0);
 }
 
 function generate(wordAmount) {

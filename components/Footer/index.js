6855e6d0b605f3c3bccc8e2b899fb7195342256a
Diego Figueroa <diego@dfigueroa.me>
2020-12-27 16:03:54 -0400

    :art: (components/Footer) add credit to icon designers

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,9 +1,21 @@
+import classNames from 'classnames';
 import styles from './Footer.module.css';
 
-export default function Footer () {
+export default function Footer ({ theme }) {
   return (
-    <footer className={styles.footer}>
+    <footer className={classNames(styles.footer, 'footer')}>
       <img src="https://madewithlove.now.sh/pr?heart=true" alt="Made with love in Puerto Rico" />
+      <div>
+        Icons made by <a href="https://www.flaticon.com/authors/smalllikeart" title="smalllikeart">smalllikeart</a> and <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
+      </div>
+      <style jsx>{`
+        .footer {
+          color: ${theme.text};
+        }
+        a:hover {
+          color: ${theme.highlight}
+        }
+      `}</style>
     </footer>
   );
 }
